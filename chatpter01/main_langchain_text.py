d94c1147c3d8e8172c2cdd4eda3b3898b18bfe53
Aiden Yue <2687927133@qq.com>
2025-07-08 23:09:58 +0800

    完成LangChain调用DeepSeek Chat接口

--- chatpter01/main_langchain_text.py
+++ chatpter01/main_langchain_text.py
@@ -1,13 +1,18 @@
 from dotenv import load_dotenv
-from langchain.llms import OpenAIChat
+from langchain_deepseek import ChatDeepSeek
 
 load_dotenv(dotenv_path="../.env")
 
-llm = OpenAIChat(
-    openai_api_base="https://api.deepseek.com/beta",
+llm = ChatDeepSeek(
     model="deepseek-chat",
     temperature=0.85,
     max_tokens=60,
 )
-response = llm.invoke("请给我的花店起一个名字")
+
+messages = [
+    {"role": "system", "content": "你是一个智能AI助手"},
+    {"role": "user", "content": "请给我的花店起一个名字"},
+]
+
+response = llm.invoke(messages)
 print(response)
